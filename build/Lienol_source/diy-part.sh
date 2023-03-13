5086ddb0dc661580c55262c571d035bee9adfe85
m215owen <39959893+m215owen@users.noreply.github.com>
2023-03-13 15:50:33 +0800

    Update diy-part.sh

--- build/Lienol_source/diy-part.sh
+++ build/Lienol_source/diy-part.sh
@@ -14,6 +14,10 @@
 #sed -i '$a src-git bypass https://github.com/kiddin9/openwrt-bypass' feeds.conf.default
 
 
+#添加主页的CPU温度显示
+sed -i "/<tr><td width=\"33%\"><%:Load Average%>/a \ \t\t<tr><td width=\"33%\"><%:CPU Temperature%></td><td><%=luci.sys.exec(\"sed 's/../&./g' /sys/class/thermal/thermal_zone0/temp|cut -c1-4\")%></td></tr>" feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+cat feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm |grep Temperature
+echo "Add CPU Temperature in Admin Index OK====================="
 
 
 cat >$NETIP <<-EOF
